a6af5999a4abc4eeccb0107108dd3c136ef2a122
Seth Green <seth.green@insight.com>
2022-02-28 14:52:04 -0500

    added more Game tables/data models/scripts

---
+++ src/data_models/GameReturnStatsData.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from sqlalchemy import (
+    Column,
+    Float,
+    Integer,
+    String
+)
+from sqlalchemy.sql.schema import ForeignKey
+
+from src.constants import Base
+
+
+@dataclass
+class GameReturnStatsData(Base):
+    __tablename__ = 'game_return_stats'
+    __table_args__ = {'extend_existing': True}
+    id = Column(String(50), primary_key=True)
+    player_id = Column(Integer, ForeignKey('player_info.id'))
+    week = Column(Integer)
+    year = Column(String(10))
+    games_played = Column(Integer)
+    kick_returns = Column(Integer)
+    kr_yds = Column(Integer)
+    kr_avg = Column(Float)
+    kr_tds = Column(Integer)
+    long_kr = Column(Integer)
+    punt_returns = Column(Integer)
+    pr_yds = Column(Integer)
+    pr_avg = Column(Float)
+    pr_tds = Column(Integer)
+    long_pr = Column(Integer)
