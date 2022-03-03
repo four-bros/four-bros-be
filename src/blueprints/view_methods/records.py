a227b6ee75d1602d92dd31f3e0db777bda3b9cd1
Seth Green <seth.green@insight.com>
2022-03-03 09:19:40 -0500

    fixed team player stats bug. Updated DB data

--- src/blueprints/view_methods/records.py
+++ src/blueprints/view_methods/records.py
@@ -14,6 +14,42 @@ from src.utils.career_stats_leaders import (
     _get_career_return_stats_leaders,
     _get_career_rush_stats_leaders
 )
+from src.utils.game_stats_leaders import (
+    _get_game_defensive_stats_leaders,
+    _get_game_kicking_stats_leaders,
+    _get_game_passing_stats_leaders,
+    _get_game_rec_stats_leaders,
+    _get_game_return_stats_leaders,
+    _get_game_rush_stats_leaders
+)
+
+
+def get_career_records(request):
+
+    response = {
+        'defense': _get_career_defensive_stats_leaders(),
+        'kicking': _get_career_kicking_stats_leaders(),
+        'passing': _get_career_passing_stats_leaders(),
+        'receiving': _get_career_rec_stats_leaders(),
+        'return': _get_career_return_stats_leaders(),
+        'rushing': _get_career_rush_stats_leaders()
+    }
+
+    return response
+
+
+def get_game_records(request):
+
+    response = {
+        'defense': _get_game_defensive_stats_leaders(),
+        'kicking': _get_game_kicking_stats_leaders(),
+        'passing': _get_game_passing_stats_leaders(),
+        'receiving': _get_game_rec_stats_leaders(),
+        'return': _get_game_return_stats_leaders(),
+        'rushing': _get_game_rush_stats_leaders()
+    }
+
+    return response
 
 
 def get_season_records(request):
@@ -30,15 +66,4 @@ def get_season_records(request):
     return response
 
 
-def get_career_records(request):
 
-    response = {
-        'defense': _get_career_defensive_stats_leaders(),
-        'kicking': _get_career_kicking_stats_leaders(),
-        'passing': _get_career_passing_stats_leaders(),
-        'receiving': _get_career_rec_stats_leaders(),
-        'return': _get_career_return_stats_leaders(),
-        'rushing': _get_career_rush_stats_leaders()
-    }
-
-    return response
