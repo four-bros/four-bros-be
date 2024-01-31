d41afe41e334dcb98f3a25fc8b9c5ef7fbf615cd
Seth Green <seth.green@insight.com>
2024-01-31 15:27:52 -0500

    added user only teams endpoints

--- src/blueprints/view_methods/teams.py
+++ src/blueprints/view_methods/teams.py
@@ -11,7 +11,8 @@ from src.constants import (
     receiving_stats_schema,
     rushing_stats_schema,
     session,
-    total_stats_schema
+    total_stats_schema,
+    user_team_ids
 )
 from src.data_models.SeasonDefensiveStatsData import SeasonDefensiveStatsData
 from src.data_models.SeasonKickingStatsData import SeasonKickingStatsData
@@ -80,7 +81,8 @@ def get_all_teams():
 
     return response
 
-def get_team_details(team_id) -> TeamDetailsSchema:
+
+def get_team_details_by_id(team_id) -> TeamDetailsSchema:
     team_info_data: TeamInfoData = session.query(
         TeamInfoData).where(TeamInfoData.id == team_id).one()
 
@@ -96,7 +98,28 @@ def get_team_details(team_id) -> TeamDetailsSchema:
 
     return response
 
-def get_team_roster(team_id) -> TeamRosterSchema:
+
+def get_user_teams_details() -> [TeamDetailsSchema]:
+    response = {}
+
+    for team_id in user_team_ids:
+        team_info_data: TeamInfoData = session.query(
+            TeamInfoData).where(TeamInfoData.id == team_id).one()
+
+        players: List[PlayerInfoData] = session.query(PlayerInfoData).where(
+            PlayerInfoData.team_id == team_id,
+            PlayerInfoData.is_active == True,
+        ).order_by(desc(PlayerInfoData.overall)).all()
+
+        team_details: TeamDetails = _get_team_details(
+            team_info=team_info_data, players=players)
+
+        response[team_details.team_name] = team_details_schema.dump(team_details)
+
+    return response
+
+
+def get_team_roster_by_id(team_id) -> TeamRosterSchema:
 
     players: List[PlayerInfoData] = session.query(PlayerInfoData).where(
         PlayerInfoData.team_id == team_id,
@@ -105,15 +128,16 @@ def get_team_roster(team_id) -> TeamRosterSchema:
 
     team_roster: List[TeamRoster] = [
         _get_team_roster(player) for player in players]
-    
-    team_roster_summary: TeamRosterSummary = TeamRosterSummary(roster=team_roster)
+
+    team_roster_summary: TeamRosterSummary = TeamRosterSummary(
+        roster=team_roster)
 
     response: TeamRosterSchema = team_roster_schema.dump(team_roster_summary)
 
     return response
 
 
-def get_team_player_stats(team_id):
+def get_team_player_stats_by_id(team_id):
     # Query the year to filter out irrelevant years
     week_year: WeekYearData = session.query(WeekYearData).order_by(
         desc(WeekYearData.year),
@@ -209,6 +233,150 @@ def get_team_player_stats(team_id):
         SeasonReturnStatsData.year == week_year.year,
         SeasonReturnStatsData.punt_returns > 0
     ).order_by(desc(SeasonReturnStatsData.pr_yds)).all()
+
+    # Convert players to the appropriate models based on stat category
+    defensive_stats: List[PlayerDefensiveStats] = [
+        _get_player_season_defensive_stats(player) for player in defense_data]
+    defensive_to_stats: List[PlayerDefensiveStats] = [
+        _get_player_season_defensive_stats(player) for player in defense_to_data]
+    kick_return_stats: List[PlayerKickingStats] = [
+        _get_player_kick_return_stats(player) for player in kick_return_data]
+    kick_stats: List[PlayerKickingStats] = [
+        _get_player_season_kicking_stats(player) for player in kicking_data]
+    passing_stats: List[PlayerPassingStats] = [
+        _get_player_season_passing_stats(player) for player in passing_data]
+    punt_return_stats: List[PlayerPuntReturnStats] = [
+        _get_player_season_punt_return_stats(player) for player in punt_return_data]
+    punt_stats: List[PlayerPuntingStats] = [
+        _get_player_season_punting_stats(player) for player in punting_data]
+    receiving_stats: List[PlayerReceivingStats] = [
+        _get_player_season_receiving_stats(player) for player in receiving_data]
+    rushing_stats: List[PlayerRushingStats] = [
+        _get_player_rushing_stats(player) for player in rushing_data]
+    total_off_stats: List[PlayerTotalStats] = [
+        _get_player_season_total_off_stats(player) for player in total_off_data]
+
+    # Convert players to json for response
+    defensive_stats_json = defensive_stats_schema.dump(defensive_stats)
+    defensive_to_stats_json = defensive_stats_schema.dump(defensive_to_stats)
+    kicking_stats_json = kicking_stats_schema.dump(kick_stats)
+    kick_return_stats_json = kick_return_stats_schema.dump(kick_return_stats)
+    passing_stats_json = passing_stats_schema.dump(passing_stats)
+    punt_return_json = punt_return_stats_schema.dump(punt_return_stats)
+    punting_stats_json = punting_stats_schema.dump(punt_stats)
+    receiving_stats_json = receiving_stats_schema.dump(receiving_stats)
+    rushing_stats_json = rushing_stats_schema.dump(rushing_stats)
+    total_stats_json = total_stats_schema.dump(total_off_stats)
+
+    response = {
+        'defense': defensive_stats_json,
+        'defense_to': defensive_to_stats_json,
+        'kicking': kicking_stats_json,
+        'kick_return': kick_return_stats_json,
+        'passing': passing_stats_json,
+        'punting': punting_stats_json,
+        'punt_return': punt_return_json,
+        'receiving': receiving_stats_json,
+        'rushing': rushing_stats_json,
+        'total': total_stats_json
+    }
+
+    return response
+
+
+def get_user_teams_player_stats():
+    # Query the year to filter out irrelevant years
+    week_year: WeekYearData = session.query(WeekYearData).order_by(
+        desc(WeekYearData.year),
+        desc(WeekYearData.week)
+    ).first()
+
+    # Get defensive stats
+    defense_data = session.query(PlayerInfoData, SeasonDefensiveStatsData).filter(
+        PlayerInfoData.team_id.in_(user_team_ids),
+        PlayerInfoData.id == SeasonDefensiveStatsData.player_id,
+        PlayerInfoData.is_active == True,
+        SeasonDefensiveStatsData.year == week_year.year,
+        SeasonDefensiveStatsData.total_tkls > 0
+    ).order_by(desc(SeasonDefensiveStatsData.total_tkls)).limit(10)
+
+    defense_to_data = session.query(PlayerInfoData, SeasonDefensiveStatsData).filter(
+        PlayerInfoData.team_id.in_(user_team_ids),
+        PlayerInfoData.id == SeasonDefensiveStatsData.player_id,
+        PlayerInfoData.is_active == True,
+        SeasonDefensiveStatsData.year == week_year.year,
+        SeasonDefensiveStatsData.total_tkls > 0
+    ).order_by(
+        desc(SeasonDefensiveStatsData.ints_made),
+        desc(SeasonDefensiveStatsData.fumbles_rec)
+    ).limit(10)
+
+    # Get offensive stats
+    passing_data = session.query(PlayerInfoData, SeasonOffensiveStatsData).filter(
+        PlayerInfoData.team_id.in_(user_team_ids),
+        PlayerInfoData.id == SeasonOffensiveStatsData.player_id,
+        PlayerInfoData.is_active == True,
+        SeasonOffensiveStatsData.year == week_year.year,
+        SeasonOffensiveStatsData.pass_att > 0
+    ).order_by(desc(SeasonOffensiveStatsData.pass_yards)).limit(5)
+
+    receiving_data = session.query(PlayerInfoData, SeasonOffensiveStatsData).filter(
+        PlayerInfoData.team_id.in_(user_team_ids),
+        PlayerInfoData.id == SeasonOffensiveStatsData.player_id,
+        PlayerInfoData.is_active == True,
+        SeasonOffensiveStatsData.year == week_year.year,
+        SeasonOffensiveStatsData.receptions > 0
+    ).order_by(desc(SeasonOffensiveStatsData.rec_yards)).limit(10)
+
+    rushing_data = session.query(PlayerInfoData, SeasonOffensiveStatsData).filter(
+        PlayerInfoData.team_id.in_(user_team_ids),
+        PlayerInfoData.id == SeasonOffensiveStatsData.player_id,
+        PlayerInfoData.is_active == True,
+        SeasonOffensiveStatsData.year == week_year.year,
+        SeasonOffensiveStatsData.rush_att > 0
+    ).order_by(desc(SeasonOffensiveStatsData.rush_yards)).limit(10)
+
+    total_off_data = session.query(PlayerInfoData, SeasonOffensiveStatsData).filter(
+        PlayerInfoData.team_id.in_(user_team_ids),
+        PlayerInfoData.id == SeasonOffensiveStatsData.player_id,
+        PlayerInfoData.is_active == True,
+        SeasonOffensiveStatsData.year == week_year.year,
+        SeasonOffensiveStatsData.total_yards > 0
+    ).order_by(desc(SeasonOffensiveStatsData.total_yards)).limit(5)
+
+    # Get kicking stats
+    kicking_data = session.query(PlayerInfoData, SeasonKickingStatsData).filter(
+        PlayerInfoData.team_id.in_(user_team_ids),
+        PlayerInfoData.id == SeasonKickingStatsData.player_id,
+        PlayerInfoData.is_active == True,
+        SeasonKickingStatsData.year == week_year.year,
+        SeasonKickingStatsData.fg_att > 0
+    ).order_by(desc(SeasonKickingStatsData.fg_made)).limit(5)
+
+    punting_data = session.query(PlayerInfoData, SeasonKickingStatsData).filter(
+        PlayerInfoData.team_id.in_(user_team_ids),
+        PlayerInfoData.id == SeasonKickingStatsData.player_id,
+        PlayerInfoData.is_active == True,
+        SeasonKickingStatsData.year == week_year.year,
+        SeasonKickingStatsData.number_punts > 0
+    ).order_by(desc(SeasonKickingStatsData.total_punt_yards)).limit(5)
+
+    # Get return stats
+    kick_return_data = session.query(PlayerInfoData, SeasonReturnStatsData).filter(
+        PlayerInfoData.team_id.in_(user_team_ids),
+        PlayerInfoData.id == SeasonReturnStatsData.player_id,
+        PlayerInfoData.is_active == True,
+        SeasonReturnStatsData.year == week_year.year,
+        SeasonReturnStatsData.kick_returns > 0
+    ).order_by(desc(SeasonReturnStatsData.kr_yds)).limit(5)
+
+    punt_return_data = session.query(PlayerInfoData, SeasonReturnStatsData).filter(
+        PlayerInfoData.team_id.in_(user_team_ids),
+        PlayerInfoData.id == SeasonReturnStatsData.player_id,
+        PlayerInfoData.is_active == True,
+        SeasonReturnStatsData.year == week_year.year,
+        SeasonReturnStatsData.punt_returns > 0
+    ).order_by(desc(SeasonReturnStatsData.pr_yds)).limit(5)
 
     # Convert players to the appropriate models based on stat category
     defensive_stats: List[PlayerDefensiveStats] = [
